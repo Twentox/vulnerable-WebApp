2153c44c347c5841bb2934b84d6916702817282c
Twentox <erikschulze05@gmail.com>
2026-03-31 13:04:56 +0200

    adds login and index route

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
+app.secret_key = "secret"
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello Mars'
+@app.route('/index')
+def choose_mode():
+    mode = request.args.get("mode")
+    session["isAdmin"] = "false"
+    if mode: 
+        session["mode"] = mode
+        return redirect(url_for('login')) 
+    return render_template("index.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+
+    if request.methode == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()
