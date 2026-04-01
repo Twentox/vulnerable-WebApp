efb344e490eae9d496d7484f6b7a792fb406d1f8
Twentox <erikschulze05@gmail.com>
2026-04-01 15:23:08 +0200

    fixes structure

--- init.sql
+++ init.sql
@@ -1,25 +1,23 @@
-CREATE vulnapp;
-USE vulnapp; 
+USE vulnapp;
 
-
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS contact;
 
 CREATE TABLE users (  
-id INT AUTO_INCREMENT PRIMARY KEY,  
-username VARCHAR(50) NOT NULL,  
-password VARCHAR(50) NOT NULL  
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    username VARCHAR(50) NOT NULL,  
+    password VARCHAR(50) NOT NULL  
 );
 
-
 CREATE TABLE contact(
-   id INT AUTO_INCREMENT PRIMARY KEY, 
-   name VARCHAR(50) NOT NULL,
-   email VARCHAR(50) NOT NULL,
-   message VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    message VARCHAR(255) NOT NULL
 ); 
-
 
 INSERT INTO users (username, password)  
 VALUES 
 ("admin", "password"),
 ("josh", "joshisthebest"),
-("staff", "letmein")
+("staff", "letmein");
