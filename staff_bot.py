c32ebe8fdb1a21c48a8f3d6ee44f2443967ab723
Twentox <erikschulze05@gmail.com>
2026-04-07 16:34:12 +0200

    adds visit_page and wait_for_web

--- staff_bot.py
+++ staff_bot.py
@@ -1,28 +1,44 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import time
+import requests
 
-def visit_page(url):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/chromium"
+def wait_for_web():
+    while True:
+        try:
+            r = requests.get("http://web:5000")
+            if r.status_code == 200:
+                print("Webserver ready!")
+                break
+        except:
+            print("Waiting for webserver...")
+        time.sleep(2)
 
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service,options=chrome_options)
+def visit_page():
+    options = Options()
+    options.add_argument("--headless")
 
+    driver = webdriver.Firefox(options=options)
+
+    driver.get("http://web:5000")
 
     driver.add_cookie({
-            "name": "session",
-            "value": "admin-session-token",
-        })
+    "name": "session",
+    "value": "eyJtb2RlIjoidW5zZWN1cmUiLCJyb2xlIjoic3RhZmYifQ.adUS8w.FpTmyggnoK__1y7w5bSxKGirE-Q"
+    })
+
+    driver.get("http://web:5000/staff")
+
+    driver.close()
 
 
-    try:
-        driver.get(url)
+if __name__ == "__main__":
+    wait_for_web()
 
-    finally:
-        driver.quit()
+    while True:
+        visit_page()
+        time.sleep(60)
+
 
